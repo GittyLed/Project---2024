4c8f7f406a57c177f217b64acd71d99a467488a7
GittyLed <gled9565@gmail.com>
2024-05-22 17:00:07 +0300

    script with relative paths

--- run.ps1
+++ run.ps1
@@ -1,32 +1,14 @@
 # Change to the directory of your C# server
-#Set-Location ./Server
-
-# Run the C# server (assuming you're using dotnet)
-#Start-Process "dotnet" -ArgumentList "run --project Server/Server.csproj" -NoNewWindow
-
-
-
-# Change to the directory of your React client
-#Set-Location ./Client/react-project-2024
-
-# Install dependencies if not already installed
-#npm install
-
-# Run the React client
-#npm start
-
-
-# Change to the directory of your C# server
 Set-Location "./Server"
 
-# Run the C# server (assuming you're using dotnet)
+# Run the C# server
 Start-Process "dotnet" -ArgumentList "run --project Server/Server.csproj" -NoNewWindow
 
 # Change to the directory of your React client
 Set-Location "../Client/react-project-2024"
 
 # Install dependencies if not already installed
-npm install
+npm i
 
 # Run the React client
 npm start
